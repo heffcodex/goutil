5c5c6457963d185771408df35aa43225ed809472
heffcodex <dev@heffcodex.pro>
2022-12-04 22:08:17 +0400

    add ArrayIntersect and Min, Max for generic numbers

--- array.go
+++ array.go
@@ -32,6 +32,32 @@ func ArrayMap[T, V any](arr []T, mapFn func(item T) V) []V {
 	return res
 }
 
+func ArrayIntersect[T comparable](arr1, arr2 []T) []T {
+	if len(arr1) == 0 || len(arr2) == 0 {
+		return nil
+	}
+
+	minArr, maxArr := arr1, arr2
+	if len(arr1) > len(arr2) {
+		minArr, maxArr = arr2, arr1
+	}
+
+	m := make(map[T]struct{}, len(minArr))
+	res := make([]T, 0, len(minArr))
+
+	for _, item := range minArr {
+		m[item] = struct{}{}
+	}
+
+	for _, item := range maxArr {
+		if _, ok := m[item]; ok {
+			res = append(res, item)
+		}
+	}
+
+	return res
+}
+
 func InArray[T comparable](arr []T, v T) bool {
 	for _, item := range arr {
 		if item == v {
